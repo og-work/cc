bc72b028fa26e12bb967821ae34d3740fd1cafea
og-work <og.work2786@gmail.com>
2017-07-10 20:08:49 +0530

    Add files via upload
    Added code for reducing the training data for aec and cc. Features not analysed yet. Error not reducing for aec and cc. Using 'mse' loss now.

--- get_data_for_cc.py
+++ get_data_for_cc.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pdb
+import scipy.io
 
 class output_cc:
 	input_train = np.array([])
@@ -20,23 +21,33 @@ class input_cc:
 	def function(self):
 		print("This is input_cc class")
 
-def function_get_training_data_cc():
+def function_get_training_data_cc(obj_input_cc):
+	#pdb.set_trace()
+	classI = obj_input_cc.classI
+	classJ = obj_input_cc.classJ
+	TRAIN_VALIDATION_SPLIT = obj_input_cc.train_valid_split
+	dataset_labels = obj_input_cc.dataset_labels
+	visual_features_dataset = obj_input_cc.visual_features
 
 	indices_input_sample_train = np.array([])
 	indices_output_sample_train = np.array([])
 	indices_input_sample_valid = np.array([])
 	indices_output_sample_valid = np.array([])
 	print "**************************************"
-	
+	MAX_NUMBER_OF_SAMPLES_CLASSI_TRAIN = 25
+	MAX_NUMBER_OF_SAMPLES_CLASSJ_TRAIN = 25
+
 	if classI != classJ:
 		indices_classI_samples = np.flatnonzero(dataset_labels == classI)
 		indices_classJ_samples = np.flatnonzero(dataset_labels == classJ)
-		number_of_samples_classI_for_train = int(TRAIN_VALIDATION_SPLIT * np.size(indices_classI_samples))
-		number_of_samples_classJ_for_train = int(TRAIN_VALIDATION_SPLIT * np.size(indices_classJ_samples))
+		#number_of_samples_classI_for_train = int(TRAIN_VALIDATION_SPLIT * np.size(indices_classI_samples))
+		#number_of_samples_classJ_for_train = int(TRAIN_VALIDATION_SPLIT * np.size(indices_classJ_samples))
+		number_of_samples_classI_for_train = MAX_NUMBER_OF_SAMPLES_CLASSI_TRAIN
+		number_of_samples_classJ_for_train = MAX_NUMBER_OF_SAMPLES_CLASSJ_TRAIN
 		indices_classI_samples_train = indices_classI_samples[:number_of_samples_classI_for_train]
-		indices_classI_samples_valid = indices_classI_samples[number_of_samples_classI_for_train:]
+		indices_classI_samples_valid = indices_classI_samples[number_of_samples_classI_for_train: 2 * number_of_samples_classI_for_train]
 		indices_classJ_samples_train = indices_classJ_samples[:number_of_samples_classJ_for_train]
-		indices_classJ_samples_valid = indices_classJ_samples[number_of_samples_classJ_for_train:]
+		indices_classJ_samples_valid = indices_classJ_samples[number_of_samples_classJ_for_train: 2 * number_of_samples_classJ_for_train]
 		print "classI %d classJ %d indices %d %d %d %d" %(classI, classJ, indices_classI_samples.size, indices_classJ_samples.size, \
 				number_of_samples_classI_for_train, number_of_samples_classJ_for_train)
 		#print(indices_classI_samples)
@@ -46,14 +57,15 @@ def function_get_training_data_cc():
 		#print(indices_classJ_samplesTrain)
 		#print(indices_classJ_samplesValid)
 	
-		#Prepare train data for CC
+		#Prepare train data for cc
 		for index_classI_sample in indices_classI_samples_train:
 			indices_classI_sample_array = np.empty(indices_classJ_samples_train.size)
 			indices_classI_sample_array.fill(index_classI_sample)
 			indices_input_sample_train = np.concatenate((indices_input_sample_train, indices_classI_sample_array), axis = 0)
 			indices_output_sample_train = np.concatenate((indices_output_sample_train, indices_classJ_samples_train), axis = 0)
 
-		#Prepare validation data for CC
+		#pdb.set_trace()
+		#Prepare validation data for cc
 		for index_classI_sample in indices_classI_samples_valid:
 			indices_classI_sample_array = np.empty(indices_classJ_samples_valid.size)
 			indices_classI_sample_array.fill(index_classI_sample)
@@ -64,15 +76,70 @@ def function_get_training_data_cc():
 			#crossCodersTrainDataInput.append(indices_input_sample_train)							
 			#crossCodersTrainDataOutput.append(indices_output_sample_train)							
 			
-		input_samples_for_CC_train = visual_features_dataset[indices_input_sample_train.astype(int), :]
-		output_samples_for_CC_train = visual_features_dataset[indices_output_sample_train.astype(int), :]
-		input_samples_for_CC_valid = visual_features_dataset[indices_input_sample_valid.astype(int), :]
-		outpt_samples_for_CC_valid = visual_features_dataset[indices_output_sample_valid.astype(int), :]
+		#pdb.set_trace()
+		input_samples_for_cc_train = visual_features_dataset[indices_input_sample_train.astype(int), :]
+		output_samples_for_cc_train = visual_features_dataset[indices_output_sample_train.astype(int), :]
+		input_samples_for_cc_valid = visual_features_dataset[indices_input_sample_valid.astype(int), :]
+		output_samples_for_cc_valid = visual_features_dataset[indices_output_sample_valid.astype(int), :]
 		
+		#pdb.set_trace()
 		obj_output_cc = output_cc()
-		obj_output_cc.input_train = input_samples_for_CC_train
-		obj_output_cc.output_train = output_samples_for_CC_train
-		obj_output_cc.input_valid = input_samples_for_CC_valid
-		obj_output_cc.output_valid = output_samples_for_CC_valid
+		obj_output_cc.input_train = input_samples_for_cc_train
+		obj_output_cc.output_train = output_samples_for_cc_train
+		obj_output_cc.input_valid = input_samples_for_cc_valid
+		obj_output_cc.output_valid = output_samples_for_cc_valid
 
 		return obj_output_cc
+
+class input_data:
+	dataset_name = []
+	system_type = []
+	train_class_labels = np.array([])
+	test_class_labels = np.array([])
+	visual_features_dataset = np.array([])
+	attributes = np.array([])
+	dataset_labels = np.array([])
+	def function(self):
+		print("This is the input_data class")	
+
+def get_input_data(obj_input_data):
+
+	if obj_input_data.system_type == 'desktop':
+		BASE_PATH = "/nfs4/omkar/Documents/"
+	else:
+		BASE_PATH = "/media/omkar/windows-D-drive/"
+	print(BASE_PATH)
+	if obj_input_data.dataset_name == 'apy':
+		path_CNN_features = BASE_PATH + "/study/phd-research/data/code-data/semantic-similarity/cnn-features/aPY/cnn_feat_imagenet-vgg-verydeep-19.mat"
+		path_attributes = BASE_PATH + "/study/phd-research/data/code-data/semantic-similarity/cnn-features/aPY/class_attributes.mat"
+		features = scipy.io.loadmat(path_CNN_features)
+		attributes_data = scipy.io.loadmat(path_attributes)
+		attributes = attributes_data['class_attributes']
+		dataset_labels = attributes_data['labels']
+		visual_features_dataset = features['cnn_feat']
+		visual_features_dataset = visual_features_dataset.transpose()
+	    #train_class_labels = np.arange(1, 33, 1)
+		train_class_labels = np.array([7, 8, 10, 12, 13, 17, 21, 22, 23, 24])
+		test_class_labels = np.arange(21, 33, 1)
+	    #'1 aeroplane' '2 bicycle''3 bird''4 boat''5 bottle''6 bus''7 car''8 cat''9 chair''10 cow''11 diningtable''12 dog''13 horse'
+	    #'14 motorbike''15 person''16 pottedplant''17 sheep''18 sofa''19 train''20 tvmonitor''21 donkey''22 monkey''23 goat''24 wolf'
+    	#'25 jetski''26 zebra''27 centaur''28 mug''29 statue''30 building''31 bag''32 carriage'
+	else:
+		dataset_labels = np.array([1, 1, 1, 1, 1, 2, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 2, 3])
+		attributes = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
+		visual_features_dataset = np.array([[11, 14, 15, 16, 17, 21, 12, 24, 25, 31, 34, 36, 37, 38, 39, 32, 22, 33],
+	                                      [11, 14, 15, 16, 17, 21, 12, 24, 25, 31, 34, 36, 37, 38, 39, 32, 22, 33],
+                                      [11, 14, 15, 16, 17, 21, 12, 24, 25, 31, 34, 36, 37, 38, 39, 32, 22, 33],
+                                      [11, 14, 15, 16, 17, 21, 12, 24, 25, 31, 34, 36, 37, 38, 39, 32, 22, 33],
+                                      [11, 14, 15, 16, 17, 21, 12, 24, 25, 31, 34, 36, 37, 38, 39, 32, 22, 33]])
+		visual_features_dataset = visual_features_dataset.transpose()
+		train_class_labels = np.array([1, 3])
+		test_class_labels = np.array([2, 3])
+		
+	obj_input_data.test_class_labels = test_class_labels 
+	obj_input_data.train_class_labels = train_class_labels 
+	obj_input_data.attributes = attributes 
+	obj_input_data.visual_features_dataset = visual_features_dataset
+	obj_input_data.dataset_labels = dataset_labels
+
+	return obj_input_data
--- main-cross-encoder.py
+++ main-cross-encoder.py
@@ -1,4 +1,5 @@
-from test_function_file import function_get_training_data_cc, input_cc, output_cc
+from get_data_for_cc import function_get_training_data_cc, input_cc, output_cc
+from get_data_for_cc import input_data, get_input_data
 from keras.layers import Input, Dense
 from keras.models import Model
 import scipy.io
@@ -8,46 +9,33 @@ from keras.datasets import mnist
 import matplotlib.pyplot as plt
 import time
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
+#os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 import tensorflow as tf
 from keras.callbacks import TensorBoard
 import numpy as np
 import pdb
 print "*****************************************************************************************************************************************"
 
-SAMPLE_DATA = 1
-EPOCHS = 1
-BATCH_SIZE = 256
-USE_DESKTOP = 0;
-TRAIN_VALIDATION_SPLIT = 0.5
+SAMPLE_DATA = 0
+EPOCHS = 10000
+EPOCHS_CC = 10000
+BATCH_SIZE = 64
+BATCH_SIZE_CC = 64
+SYSTEM = 'desktop'; #desktop/laptop
+TRAIN_VALIDATION_SPLIT = 0.7
+MIN_NUMBER_OF_SAMPLES_ACROSS_CLASSES = 50
 
-if USE_DESKTOP:
-	BASE_PATH = '/nfs4/omkar/Documents/'
-else:
-	BASE_PATH = '/media/omkar/windows-D-drive/'
+obj_input_data = input_data()
+obj_input_data.dataset_name = 'apy'
+obj_input_data.system_type = SYSTEM
 
-if 0 == SAMPLE_DATA:
-	path_CNN_features = BASE_PATH + "/study/phd-research/data/code-data/semantic-similarity/cnn-features/aPY/cnn_feat_imagenet-vgg-verydeep-19.mat"
-	path_attributes = BASE_PATH + "/study/phd-research/data/code-data/semantic-similarity/cnn-features/aPY/class_attributes.mat"
-	features = scipy.io.loadmat(path_CNN_features)
-	attributes_data = scipy.io.loadmat(path_attributes)
-	attributes = attributes_data['class_attributes']
-	dataset_labels = attributes_data['labels']
-	visual_features_dataset = features['cnn_feat']
-	visual_features_dataset = visual_features_dataset.transpose()
-	train_class_labels = np.arange(1, 33, 1)
-	test_class_labels = np.arange(21, 33, 1)
-else:
-	dataset_labels = np.array([1, 1, 1, 1, 1, 2, 1, 2, 2, 3, 3, 3, 3, 3, 3, 3, 2, 3])
-	attributes = np.array([[1, 2, 3], [1, 2, 3], [1, 2, 3]])
-	visual_features_dataset = np.array([[11, 14, 15, 16, 17, 21, 12, 24, 25, 31, 34, 36, 37, 38, 39, 32, 22, 33],
-									  [11, 14, 15, 16, 17, 21, 12, 24, 25, 31, 34, 36, 37, 38, 39, 32, 22, 33],
-									  [11, 14, 15, 16, 17, 21, 12, 24, 25, 31, 34, 36, 37, 38, 39, 32, 22, 33],
-									  [11, 14, 15, 16, 17, 21, 12, 24, 25, 31, 34, 36, 37, 38, 39, 32, 22, 33],
-									  [11, 14, 15, 16, 17, 21, 12, 24, 25, 31, 34, 36, 37, 38, 39, 32, 22, 33]])
-	visual_features_dataset = visual_features_dataset.transpose()
-	train_class_labels = np.array([1, 3])
-	test_class_labels = np.array([2, 3])
+obj_input_data = get_input_data(obj_input_data)
+
+visual_features_dataset = obj_input_data.visual_features_dataset
+train_class_labels = obj_input_data.train_class_labels
+test_class_labels = obj_input_data.test_class_labels
+attributes = obj_input_data.attributes
+dataset_labels = obj_input_data.dataset_labels
 
 number_of_train_classes = np.size(train_class_labels)
 number_of_test_classes = np.size(test_class_labels)
@@ -71,10 +59,10 @@ valid_sample_labels = np.array([])
 
 for class_index in train_class_labels: 
 	indices = np.flatnonzero(dataset_labels == class_index)
-	#number_of_samples_for_train = TRAIN_VALIDATION_SPLIT * MIN_NUMBER_OF_SAMPLES_ACROSS_CLASSES
-	number_of_samples_for_train = int(TRAIN_VALIDATION_SPLIT * np.size(indices))
+	number_of_samples_for_train = int(TRAIN_VALIDATION_SPLIT * MIN_NUMBER_OF_SAMPLES_ACROSS_CLASSES)
+#	number_of_samples_for_train = int(TRAIN_VALIDATION_SPLIT * np.size(indices))
 	indices_train = indices[:number_of_samples_for_train]
-	indices_valid = indices[number_of_samples_for_train:]
+	indices_valid = indices[number_of_samples_for_train:MIN_NUMBER_OF_SAMPLES_ACROSS_CLASSES]
 	train_sample_indices = np.concatenate((train_sample_indices, indices_train), axis = 0)
 	valid_sample_indices = np.concatenate((valid_sample_indices, indices_valid), axis = 0)
 	train_labels = np.empty(number_of_samples_for_train)
@@ -114,9 +102,9 @@ if SAMPLE_DATA:
 	print(test_sample_indices)
 
 #Prepare encoder model...................
-dimension_hidden_layer1 = 1000
-dimension_hidden_layer2 = 500
-dimension_hidden_layer3 = 300
+dimension_hidden_layer1 = 2000
+dimension_hidden_layer2 = 1000
+dimension_hidden_layer3 = 500
 
 #...............stacked AECs...........
 #...................AEC 1..............
@@ -137,7 +125,7 @@ encoded_input1 = Input(shape=(dimension_hidden_layer1,))
 decoder_layer1 = aec1.layers[-1]
 # create the decoder model
 decoder1 = Model(encoded_input1, decoder_layer1(encoded_input1))
-aec1.compile(optimizer='adadelta', loss='binary_crossentropy')
+aec1.compile(optimizer='adadelta', loss='mse')
 aec_start = time.time()
 aec1.fit(train_samples, train_samples,
                 epochs=EPOCHS,
@@ -146,6 +134,7 @@ aec1.fit(train_samples, train_samples,
                 validation_data=(valid_samples, valid_samples),
 		callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
 aec_end = time.time()
+aec1_time = aec_end - aec_start
 print "aec training time: %f"%((aec_end - aec_start)/60) 
 
 # encode and decode some digits
@@ -157,8 +146,14 @@ encoded_data_valid1 = encoder1.predict(valid_samples)
 decoded_data_valid1 = decoder1.predict(encoded_data_valid1)
 
 scipy.io.savemat('data/aec1_encoded.mat', \
-	dict(encoded_data_train1 = encoded_data_train1, encoded_data_valid1 = encoded_data_valid1))
-pdb.set_trace()
+	dict(encoded_data_train1 = encoded_data_train1,\
+	     encoded_data_valid1 = encoded_data_valid1, \
+	     encoded_data_test1 = encoded_data_test1, \
+	     decoded_data_train1 = decoded_data_train1, \
+	     decoded_data_valid1 = decoded_data_valid1, \
+	     decoded_data_test1 = decoded_data_test1, \
+	     	))
+print("Save encoded/decoded data for aec1.")
 
 #Save model
 #Serialize model to JSON
@@ -187,8 +182,9 @@ encoded_input2 = Input(shape=(dimension_hidden_layer2,))
 decoder_layer2 = aec2.layers[-1]
 # create the decoder model
 decoder2 = Model(encoded_input2, decoder_layer2(encoded_input2))
-aec2.compile(optimizer='adadelta', loss='binary_crossentropy')
+aec2.compile(optimizer='adadelta', loss='mse')
 aec_start = time.time()
+pdb.set_trace()
 aec2.fit(encoded_data_train1, encoded_data_train1,
                 epochs=EPOCHS,
                 batch_size=256,
@@ -197,6 +193,7 @@ aec2.fit(encoded_data_train1, encoded_data_train1,
 		callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
 aec_end = time.time()
 print "aec training time: %f"%((aec_end - aec_start)/60) 
+aec2_time = aec_end - aec_start
 
 # encode and decode some digits
 encoded_data_test2 = encoder2.predict(encoded_data_test1)
@@ -205,6 +202,17 @@ encoded_data_train2 = encoder2.predict(encoded_data_train1)
 decoded_data_train2 = decoder2.predict(encoded_data_train2)
 encoded_data_valid2 = encoder2.predict(encoded_data_valid1)
 decoded_data_valid2 = decoder2.predict(encoded_data_valid2)
+
+#Save data
+scipy.io.savemat('data/aec2_encoded.mat', \
+	dict(encoded_data_train2 = encoded_data_train2,\
+	     encoded_data_valid2 = encoded_data_valid2, \
+	     encoded_data_test2 = encoded_data_test2, \
+	     decoded_data_train2 = decoded_data_train2, \
+	     decoded_data_valid2 = decoded_data_valid2, \
+	     decoded_data_test2 = decoded_data_test2, \
+	     	))
+print("Save encoded/decoded data for aec2.")
 
 #Save model
 #Serialize model to JSON
@@ -233,7 +241,7 @@ encoded_input3 = Input(shape=(dimension_hidden_layer3,))
 decoder_layer3 = aec3.layers[-1]
 # create the decoder model
 decoder3 = Model(encoded_input3, decoder_layer3(encoded_input3))
-aec3.compile(optimizer='adadelta', loss='binary_crossentropy')
+aec3.compile(optimizer='adadelta', loss='mse')
 aec_start = time.time()
 aec3.fit(encoded_data_train2, encoded_data_train2,
                 epochs=EPOCHS,
@@ -243,6 +251,7 @@ aec3.fit(encoded_data_train2, encoded_data_train2,
 		callbacks=[TensorBoard(log_dir='/tmp/autoencoder')])
 aec_end = time.time()
 print "aec training time: %f"%((aec_end - aec_start)/60) 
+aec3_time = aec_end - aec_start
 
 # encode and decode some digits
 encoded_data_test3 = encoder3.predict(encoded_data_test2)
@@ -251,6 +260,17 @@ encoded_data_train3 = encoder3.predict(encoded_data_train2)
 decoded_data_train3 = decoder3.predict(encoded_data_train3)
 encoded_data_valid3 = encoder3.predict(encoded_data_valid2)
 decoded_data_valid3 = decoder3.predict(encoded_data_valid3)
+
+#Save data
+scipy.io.savemat('data/aec3_encoded.mat', \
+	dict(encoded_data_train3 = encoded_data_train3,\
+	     encoded_data_valid3 = encoded_data_valid3, \
+	     encoded_data_test3 = encoded_data_test3, \
+	     decoded_data_train3 = decoded_data_train3, \
+	     decoded_data_valid3 = decoded_data_valid3, \
+	     decoded_data_test3 = decoded_data_test3, \
+	     	))
+print("Save encoded/decoded data for aec3.")
 
 #Save model
 #Serialize model to JSON
@@ -261,6 +281,7 @@ with open("data/aec3.json", "w") as json_file:
 aec3.save_weights("data/aec3.h5")
 print("Saved model to disk")
 
+pdb.set_trace()
 #......................cc.......................
 number_of_cc = number_of_train_classes * number_of_train_classes - number_of_train_classes
 
@@ -295,7 +316,7 @@ for classI in train_class_labels:
 			encoded_input_cc1 = Input(shape=(encoding_dimension_cc1,))
 			decoder_layer_cc1 = cc1.layers[-1]
 			decoder_cc1 = Model(encoded_input_cc1, decoder_layer_cc1(encoded_input_cc1))
-			cc1.compile(optimizer='adadelta', loss='binary_crossentropy')
+			cc1.compile(optimizer='adadelta', loss='mse')
 			
 			obj_input_cc = input_cc()
 			obj_input_cc.classI = classI
@@ -303,14 +324,16 @@ for classI in train_class_labels:
 			obj_input_cc.visual_features = visual_features_dataset
 			obj_input_cc.train_valid_split = TRAIN_VALIDATION_SPLIT
 			obj_input_cc.dataset_labels = dataset_labels
-			cc1_train_valid_data = function_get_training_data_cc(obj_input_cc)
 			
+			cc1_train_valid_data = function_get_training_data_cc(obj_input_cc)
+			cc1_start = time.time()
 			cc1.fit(cc1_train_valid_data.input_train, cc1_train_valid_data.output_train,
-            			    epochs=EPOCHS,
-			                batch_size=BATCH_SIZE,
+            			    epochs=EPOCHS_CC,
+			                batch_size=BATCH_SIZE_CC,
 			                shuffle=True,
 			                validation_data=(cc1_train_valid_data.input_valid, cc1_train_valid_data.output_valid))
-
+			cc1_end = time.time()
+			cc1_time = cc1_end - cc1_start
 			#Save models to JSON
 			modelName = 'data/cc1_' + str(classI) + '_' + str(classJ) + '.json' 
 			cc1_json = cc1.to_json()
@@ -323,7 +346,13 @@ for classI in train_class_labels:
 			#Get cc features for training samples
 			encoded_data_train_cc1 = encoder_cc1.predict(cc1_train_valid_data.input_train)	
 			encoded_data_valid_cc1 = encoder_cc1.predict(cc1_train_valid_data.input_valid)	
-		
+			
+			file_name = 'data/cc1_encoded_' + str(classI) + '_' + str(classJ) + '.mat'		
+			scipy.io.savemat(file_name, \
+				dict(encoded_data_train_cc1 = encoded_data_train_cc1,\
+				     encoded_data_valid_cc1 = encoded_data_valid_cc1,))
+			print "Save encoded/decoded data for cc1: %s" %file_name
+	 
 			#..................cc2.........................
 			encoding_dimension_cc2 = dimension_hidden_layer2
 			input_cc2 = Input(shape=(dimension_hidden_layer1,))
@@ -334,12 +363,15 @@ for classI in train_class_labels:
 			encoded_input_cc2 = Input(shape=(encoding_dimension_cc2,))
 			decoder_layer_cc2 = cc2.layers[-1]
 			decoder_cc2 = Model(encoded_input_cc2, decoder_layer_cc2(encoded_input_cc2))
-			cc2.compile(optimizer='adadelta', loss='binary_crossentropy')
+			cc2.compile(optimizer='adadelta', loss='mse')
+			cc2_start = time.time()
 			cc2.fit(encoded_data_train_cc1, encoded_data_train_cc1,
-            			    epochs=EPOCHS,
-			                batch_size=BATCH_SIZE,
+            			    epochs=EPOCHS_CC,
+			                batch_size=BATCH_SIZE_CC,
 			                shuffle=True,
 			                validation_data=(encoded_data_valid_cc1, encoded_data_valid_cc1))
+			cc2_end = time.time()
+			cc2_time = cc2_end - cc2_start
 
 			#Save models to JSON
 			modelName = 'data/cc2_' + str(classI) + '_' + str(classJ) + '.json' 
@@ -354,6 +386,12 @@ for classI in train_class_labels:
 			encoded_data_train_cc2 = encoder_cc2.predict(encoded_data_train_cc1)	
 			encoded_data_valid_cc2 = encoder_cc2.predict(encoded_data_valid_cc1)	
 
+			file_name = 'data/cc2_encoded_' + str(classI) + '_' + str(classJ) + '.mat'		
+			scipy.io.savemat(file_name, \
+				dict(encoded_data_train_cc2 = encoded_data_train_cc2,\
+				     encoded_data_valid_cc2 = encoded_data_valid_cc2,))
+			print "Saved encoded/decoded data for cc2: %s" %file_name
+
 			#...................cc3........................
 			encoding_dimension_cc3 = dimension_hidden_layer3
 			input_cc3 = Input(shape=(dimension_hidden_layer2,))
@@ -364,13 +402,15 @@ for classI in train_class_labels:
 			encoded_input_cc3 = Input(shape=(encoding_dimension_cc3,))
 			decoder_layer_cc3 = cc3.layers[-1]
 			decoder_cc3 = Model(encoded_input_cc3, decoder_layer_cc3(encoded_input_cc3))
-			cc3.compile(optimizer='adadelta', loss='binary_crossentropy')
-			
+			cc3.compile(optimizer='adadelta', loss='mse')
+			cc3_start = time.time()	
 			cc3.fit(encoded_data_train_cc2, encoded_data_train_cc2,
-            			    epochs=EPOCHS,
-			                batch_size=BATCH_SIZE,
+            			    epochs=EPOCHS_CC,
+			                batch_size=BATCH_SIZE_CC,
 			                shuffle=True,
 			                validation_data=(encoded_data_valid_cc2, encoded_data_valid_cc2))
+			cc3_end = time.time()
+			cc3_time = cc3_end - cc3_start
 
 			#Save models to JSON
 			modelName = 'data/cc3_' + str(classI) + '_' + str(classJ) + '.json' 
@@ -384,9 +424,20 @@ for classI in train_class_labels:
 			#Get cc features for training samples
 			encoded_data_train_cc3 = encoder_cc3.predict(encoded_data_train_cc2)	
 			encoded_data_valid_cc3 = encoder_cc3.predict(encoded_data_valid_cc2)	
+			
+			file_name = 'data/cc3_encoded_' + str(classI) + '_' + str(classJ) + '.mat'		
+			scipy.io.savemat(file_name, \
+				dict(encoded_data_train_cc3 = encoded_data_train_cc3,\
+				     encoded_data_valid_cc3 = encoded_data_valid_cc3,))
+			print "Saved encoded/decoded data for cc3: %s" %file_name
 			#stack features.....................................
 			#auto_cc_features_train = np.append(auto_cc_features_train, decoded_data_train_cc, axis = 1)
 			#auto_cc_features_valid = np.append(auto_cc_features_valid, decoded_data_valid_cc, axis = 1)
 			#auto_cc_features_test = np.append(auto_cc_features_test, decoded_data_test_cc, axis = 1)
 
 cc_end = time.time() 
+total_aec_time = aec1_time + aec2_time + aec3_time
+total_cc_time = cc1_time + cc2_time + cc3_time
+total_time = total_aec_time + total_cc_time
+print "Total processing time %f"%(total_time/3600)
+print
